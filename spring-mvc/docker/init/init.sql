c3cd25c14dc06e1bff6b2f5558224a31ebfc26da
CodeDDAGGI <qudrb1132@naver.com>
2025-02-08 22:38:24 +0900

    Feat : "docker compose 설정"

--- spring-mvc/docker/init/init.sql
+++ spring-mvc/docker/init/init.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE IF NOT EXISTS `payment`;
 GRANT ALL ON `payment`.* TO 'user'@'%';
 --  user가 grant라는 권한을 가짐
+
+CREATE TABLE USER(
+    id INT primary key  AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL ,
+    email VARCHAR(100) UNIQUE ,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
